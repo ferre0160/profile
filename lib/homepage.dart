0828f3100fb78f33d68ba788174bc1b61e1f8e6c
ferre0160 <heydeferre@gmail.com>
2021-12-10 15:18:47 +0100

    last changes in style + finish homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:first_project/main.dart';
 import 'package:flutter/material.dart';
 
@@ -13,12 +15,57 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: IconButton(
-        icon: Icon(Icons.create_new_folder_outlined),
-        onPressed: () {
-          Navigator.pushNamed(context, '/second');
-        },
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 200),
+            Text(
+              'WELCOME',
+              style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Create your character profile:',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            SizedBox(height: 15),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: 'Enter a name',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[850],
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.orange)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+              child: const Text('Create Profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
@@ -18,6 +20,7 @@ class HomeTab extends StatelessWidget {
           children: <Widget>[
             Center(
               child: CircleAvatar(
+                // je zou een eigen image moeten kunnen uploaden
                 backgroundImage: NetworkImage(
                     'https://images.unsplash.com/photo-1626557094739-9761f336d82b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'),
                 radius: 40,
@@ -35,9 +38,10 @@ class HomeTab extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
+            // name zou de ingevulde name uit homepage moeten worden
             Text('Thomas',
                 style: TextStyle(
-                  color: Colors.amberAccent,
+                  color: Colors.orange,
                   letterSpacing: 2,
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -45,7 +49,7 @@ class HomeTab extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            Text('AGE',
+            Text('CURRENT LEVEL',
                 style: TextStyle(
                   color: Colors.grey,
                   letterSpacing: 2,
@@ -53,9 +57,10 @@ class HomeTab extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            Text('23',
+            // level zou moeten beginnen op 1, en via de floatingbutton elke keer +1
+            Text('4',
                 style: TextStyle(
-                  color: Colors.amberAccent,
+                  color: Colors.orange,
                   letterSpacing: 2,
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -66,24 +71,32 @@ class HomeTab extends StatelessWidget {
             Row(
               children: <Widget>[
                 Icon(
-                  Icons.email,
+                  Icons.info,
                   color: Colors.grey[400],
                 ),
                 SizedBox(
                   width: 10,
                 ),
                 Text(
-                  'thomaswalsh@example.com',
+                  "Click the '+' button to enter a new level",
                   style: TextStyle(
                     color: Colors.grey[400],
-                    fontSize: 18,
+                    fontSize: 14,
                     letterSpacing: 1,
                   ),
                 )
               ],
-            )
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // isExtended: true,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.orange,
+        onPressed: () {
+          print('+ 1 level');
+        },
       ),
     );
   }
